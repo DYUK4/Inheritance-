efd7b959d0fa3df4cc9cc21204c4e14052a4f0a8
Andrey <AndreyP@home>
2025-06-18 23:50:32 +0300

    Commin

--- Academy/main.cpp
+++ Academy/main.cpp
@@ -5,6 +5,8 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+#define delimiter "\n--------------------------------------------------\n"
+
 #define HUMAN_TAKE_PARAMETERS const std::string& last_name, const std::string& first_name, int age
 #define HUMAN_GIVE_PARAMETERS last_name,first_name,age
 
@@ -31,10 +33,10 @@ public:
 		set_age(age);
 		cout << "HConstructor:\t" << this << endl;
 	}
-	~Human() { cout << "HDestructor:\t" << this << endl; }
+	virtual ~Human() { cout << "HDestructor:\t" << this << endl; }
 	
 	 // Methods:
-	void print()const
+	virtual void print()const
 	{
 		cout << last_name << " " << first_name << " " << age << endl;
 	}
@@ -71,10 +73,19 @@ public:
 		set_attendance(attendance);
 
 	}
+	Student(const Human& human, STUDENT_TAKE_PARAMETERS) :Human(human)
+	{
+		set_speciality(speciality);
+		set_group(group);
+		set_rating(rating);
+		set_attendance(attendance);
+		cout << "SConstructor:\t" << this << endl;
+
+	}
 	~Student() { cout << "SDestructor:\t" << this << endl; }
 
 	//   Methods:
-	void print()const
+	void print()const override
 	{
 		Human::print();
 		cout << speciality << " " << group << " " << rating << " " << attendance << endl;
@@ -105,7 +116,7 @@ public:
 	~Teacher() { cout << "TDestructor:\t" <<this<< endl; }
 
 	//  Methods:
-	void print()const
+	void print()const override
 	{
 		Human::print();
 		cout << speciality << " " << experience << " years" << endl;
@@ -113,18 +124,101 @@ public:
 
 };
 
+#define GRADUATE_TAKE_PARAMETERS const std::string& subject
+#define GRADUATE_GIVE_PARAMETERS subject
+
+class Graduate :public Student
+{
+	std::string subject;
+	//std::string theme;
+	//int course;
+public:
+	const std::string& get_subject()const { return subject; }
+	//const std::string& get_theme()const { return theme; }
+	//int get_course()const { return course; }
+
+	void set_subject(const std::string& subject) { this->subject = subject; }
+	//void set_theme(const std::string& theme) { this->theme = theme; }	
+	//void set_course(int course) { this->course = course; }
+
+	 // Constructor
+	Graduate(HUMAN_TAKE_PARAMETERS,STUDENT_TAKE_PARAMETERS, GRADUATE_TAKE_PARAMETERS):
+		Student(HUMAN_GIVE_PARAMETERS,STUDENT_GIVE_PARAMETERS)
+	{
+		set_subject(subject);
+		//set_theme(theme);
+		//set_course(course);
+		cout << "GConstructor:\t" << this << endl;
+	}
+	Graduate(const Student& student, GRADUATE_TAKE_PARAMETERS) :Student(student)
+	{
+		set_subject(subject);
+		cout << "G2Constructor:\t" << this << endl;
+	}
+
+	~Graduate() { cout << "GDestructor:\t" << this << endl; }
+
+	// Methods:
+	void print()const override
+	{
+		Student::print();
+		cout << subject <<endl;
+	}
+
+};
+
+//#define INHERITANCE_1
+//#define INHERITANCE_2
+
 void main()
 {
 	setlocale(LC_ALL, "");
 	cout << "HelloAcademy" << endl;
-
+#ifdef INHERITANCE_1
+	cout << delimeter;
 	Human human("Richter", "Jeffrey", 40);
 	human.print();
-
+	cout << delimiter;
 	Student student("Pinkman", "Jessie", 20, "Chenistry", "WW_220", 95, 90);
 	student.print();
-
+	cout << delimiter;
 	Teacher teacher("White", "Walter", 50, "Chimetry", 25);
 	teacher.print();
+	cout << delimiter;
+	Graduate graduate("Ivanov", "Petr", 20, "Chenistry", "WW_220", 95, 90, "Matem");
+	graduate.print();
+	cout << delimiter;
+#endif // INHERITANCE_1
+#ifdef INHERITANCE_2
+	Human human("Vercetty", "Tomy", 30);
+	human.print();
+	cout << delimiter;
+	Student student(human, "Theft", "Vice", 95, 98);
+	student.print();
+	cout << delimiter;
+	Graduate graduate(student, "How to make mony");
+	graduate.print();
+	cout << delimiter;
+#endif // INHERITANCE_2
 
+	
+	Human* group[] =
+	{
+		new Student("Pinkman", "Jessie", 20, "Chenistry", "WW_220", 95, 90),
+		new Teacher("White", "Walter", 50, "Chimetry", 25),
+		new Graduate("Ivanov", "Petr", 20, "Chenistry", "WW_220", 95, 90, "Matem"),
+		new Student("Varcetti","Tommy",30,"Theft","Vice",95,98),
+		new Teacher("Diaz","Ricardo",50,"Weapons distribution",20)
+	};
+	for (int i = 0; i <sizeof(group)/sizeof(group[0]); i++)
+	{
+		group[i]->print();
+		//cout << *group[i] << endl;
+		cout << delimiter << endl;
+	}
+	for (int i = 0; i < sizeof(group) / sizeof(group[0]); i++)
+	{
+		delete group[i];
+	}
+	
 }
