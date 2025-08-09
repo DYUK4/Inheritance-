e57d62d55aa749774d85abd6e427dff21ae4c2b7
Andrey <AndreyP@home>
2025-08-09 20:38:52 +0300

    ForwardList Iterator DONE

---
+++ AbstractGeom/main.cpp
@@ -0,0 +1,513 @@
+#define _USE_MATH_DEFINES
+#include <Windows.h>
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+namespace Geometry
+{
+    enum Color
+    {
+        CONSOLE_RED = 0xCC,
+        CONSOLE_GREEN = 0xAA,
+        CONSOLE_BLUE = 0x99,
+        CONSOLE_DEFAULT = 0x07,
+        RED = 0x000000FF,
+        GREEN = 0x0000FF00,
+        BLUE = 0x00FF0000,
+        YELLOW = 0x0000FFFF,
+    };
+
+#define SHAPE_TAKE_PARAMETERS unsigned int start_x, unsigned int start_y, unsigned int line_width, Color color
+#define SHAPE_GIVE_PARAMETERS start_x, start_y, line_width, color
+
+    class Shape
+    {
+    protected:
+        unsigned int start_x;
+        unsigned int start_y;
+        unsigned int line_width;
+        Color color;
+
+        static const int MIN_START_X = 100;
+        static const int MAX_START_X = 1000;
+        static const int MIN_START_Y = 100;
+        static const int MAX_START_Y = 500;
+        static const int MIN_LINE_WIDTH = 1;
+        static const int MAX_LINE_WIDTH = 32;
+        static const int MIN_SIZE = 50;
+        static const int MAX_SIZE = 550;
+        static int count;
+
+    public:
+        virtual double get_area() const = 0;
+        virtual double get_perimeter() const = 0;
+        virtual void draw(HDC hdc) const = 0;
+
+        Shape(SHAPE_TAKE_PARAMETERS) : start_x(start_x), start_y(start_y), line_width(line_width), color(color)
+        {
+            set_start_x(start_x);
+            set_start_y(start_y);
+            set_line_width(line_width);
+            count++;
+        }
+        virtual ~Shape()
+        {
+            count--;
+        }
+        static int get_count()
+        {
+            return count;
+        }
+
+        unsigned int get_start_x() const
+        {
+            return start_x;
+        }
+        unsigned int get_start_y() const
+        {
+            return start_y;
+        }
+        unsigned int get_line_width() const
+        {
+            return line_width;
+        }
+        void set_start_x(unsigned int start_x)
+        {
+            if (start_x < MIN_START_X) start_x = MIN_START_X;
+            if (start_x > MAX_START_X) start_x = MAX_START_X;
+            this->start_x = start_x;
+        }
+        void set_start_y(unsigned int start_y)
+        {
+            if (start_y < MIN_START_Y) start_y = MIN_START_Y;
+            if (start_y > MAX_START_Y) start_y = MAX_START_Y;
+            this->start_y = start_y;
+        }
+        void set_line_width(unsigned int line_width)
+        {
+            if (line_width < MIN_LINE_WIDTH) line_width = MIN_LINE_WIDTH;
+            if (line_width > MAX_LINE_WIDTH) line_width = MAX_LINE_WIDTH;
+            this->line_width = line_width;
+        }
+        int filter_size(int size) const
+        {
+            return
+                size < MIN_SIZE ? MIN_SIZE :
+                size > MAX_SIZE ? MAX_SIZE :
+                size;
+        }
+
+        virtual void info() const
+        {
+            cout << "Площадь фигуры: " << get_area() << endl;
+            cout << "Периметр фигуры: " << get_perimeter() << endl;
+        }
+    };
+
+    int Shape::count = 0;
+
+    class Rectangle : public Shape
+    {
+        double width;
+        double height;
+    public:
+        Rectangle(double width, double height, SHAPE_TAKE_PARAMETERS) : Shape(SHAPE_GIVE_PARAMETERS)
+        {
+            set_width(width);
+            set_height(height);
+        }
+        ~Rectangle() {}
+        void set_width(double width)
+        {
+            this->width = filter_size(width);
+        }
+        void set_height(double height)
+        {
+            this->height = filter_size(height);
+        }
+        double get_width() const
+        {
+            return width;
+        }
+        double get_height() const
+        {
+            return height;
+        }
+        double get_area() const override
+        {
+            return width * height;
+        }
+        double get_perimeter() const override
+        {
+            return (width + height) * 2;
+        }
+        void draw(HDC hdc) const override
+        {
+            HPEN hPen = CreatePen(PS_SOLID, line_width, color);
+            HBRUSH hBrush = CreateSolidBrush(color);
+
+            SelectObject(hdc, hPen);
+            SelectObject(hdc, hBrush);
+
+            ::Rectangle(hdc, start_x, start_y, start_x + width, start_y + height);
+
+            DeleteObject(hBrush);
+            DeleteObject(hPen);
+        }
+        void info() const override
+        {
+            cout << typeid(*this).name() << endl;
+            cout << "Ширина прямоугольника: " << get_width() << endl;
+            cout << "Высота прямоугольника: " << get_height() << endl;
+            Shape::info();
+        }
+    };
+
+    class Square : public Rectangle
+    {
+    public:
+        Square(double side, SHAPE_TAKE_PARAMETERS) : Rectangle(side, side, SHAPE_GIVE_PARAMETERS) {}
+        ~Square() {}
+    };
+
+    class Circle : public Shape
+    {
+        double radius;
+    public:
+        Circle(double radius, SHAPE_TAKE_PARAMETERS) : Shape(SHAPE_GIVE_PARAMETERS)
+        {
+            set_radius(radius);
+        }
+        ~Circle() {}
+        void set_radius(double radius)
+        {
+            this->radius = filter_size(radius);
+        }
+        double get_radius() const
+        {
+            return radius;
+        }
+        double get_diameter() const
+        {
+            return 2 * radius;
+        }
+        double get_area() const override
+        {
+            return M_PI * radius * radius;
+        }
+        double get_perimeter() const override
+        {
+            return 2 * M_PI * radius;
+        }
+        void draw(HDC hdc) const override
+        {
+            HPEN hPen = CreatePen(PS_SOLID, line_width, color);
+            HBRUSH hBrush = CreateSolidBrush(color);
+
+            SelectObject(hdc, hPen);
+            SelectObject(hdc, hBrush);
+
+            ::Ellipse(hdc, start_x, start_y, start_x + get_diameter(), start_y + get_diameter());
+
+            DeleteObject(hBrush);
+            DeleteObject(hPen);
+        }
+        void info() const override
+        {
+            cout << typeid(*this).name() << endl;
+            cout << "Радиус круга: " << get_radius() << endl;
+            cout << "Диаметр круга: " << get_diameter() << endl;
+            Shape::info();
+        }
+    };
+
+    class Triangle : public Shape
+    {
+    public:
+        virtual double get_height() const = 0;
+        Triangle(SHAPE_TAKE_PARAMETERS) : Shape(SHAPE_GIVE_PARAMETERS) {}
+        ~Triangle() {}
+        void info() const override
+        {
+            cout << "Высота треугольника: " << get_height() << endl;
+            Shape::info();
+        }
+    };
+
+    class EquilateralTriangle : public Triangle
+    {
+        double side;
+    public:
+        EquilateralTriangle(double side, SHAPE_TAKE_PARAMETERS) : Triangle(SHAPE_GIVE_PARAMETERS)
+        {
+            set_side(side);
+        }
+        void set_side(double side)
+        {
+            this->side = filter_size(side);
+        }
+        double get_side() const
+        {
+            return side;
+        }
+        double get_height() const override
+        {
+            return sqrt(3) / 2 * side;
+        }
+        double get_area() const override
+        {
+            return sqrt(3) / 4 * side * side;
+        }
+        double get_perimeter() const override
+        {
+            return 3 * side;
+        }
+        void draw(HDC hdc) const override
+        {
+            HPEN hPen = CreatePen(PS_SOLID, line_width, color);
+            HBRUSH hBrush = CreateSolidBrush(color);
+
+            SelectObject(hdc, hPen);
+            SelectObject(hdc, hBrush);
+
+            POINT points[] =
+            {
+                {start_x, start_y + side},
+                {start_x + side, start_y + side},
+                {start_x + side / 2, start_y + side - get_height()}
+            };
+            Polygon(hdc, points, 3);
+
+            DeleteObject(hBrush);
+            DeleteObject(hPen);
+        }
+        void info() const override
+        {
+            cout << typeid(*this).name() << endl;
+            cout << "Сторона равностороннего треугольника: " << get_side() << endl;
+            Triangle::info();
+        }
+    };
+
+    class IsoscelesTriangle : public Triangle
+    {
+        double base;
+        double height;
+    public:
+        IsoscelesTriangle(double base, double height, SHAPE_TAKE_PARAMETERS) : Triangle(SHAPE_GIVE_PARAMETERS)
+        {
+            set_base(base);
+            set_height(height);
+        }
+        void set_base(double base)
+        {
+            this->base = filter_size(base);
+        }
+        void set_height(double height)
+        {
+            this->height = filter_size(height);
+        }
+        double get_base() const
+        {
+            return base;
+        }
+        double get_height() const override
+        {
+            return height;
+        }
+        double get_area() const override
+        {
+            return base * height / 2;
+        }
+        double get_perimeter() const override
+        {
+            return base + 2 * sqrt((base / 2) * (base / 2) + height * height);
+        }
+        void draw(HDC hdc) const override
+        {
+            HPEN hPen = CreatePen(PS_SOLID, line_width, color);
+            HBRUSH hBrush = CreateSolidBrush(color);
+
+            SelectObject(hdc, hPen);
+            SelectObject(hdc, hBrush);
+
+            POINT points[] =
+            {
+                {start_x, start_y + height},
+                {start_x + base, start_y + height},
+                {start_x + base / 2, start_y}
+            };
+            Polygon(hdc, points, 3);
+
+            DeleteObject(hBrush);
+            DeleteObject(hPen);
+        }
+        void info() const override
+        {
+            cout << typeid(*this).name() << endl;
+            cout << "Основание равнобедренного треугольника: " << get_base() << endl;
+            cout << "Высота равнобедренного треугольника: " << get_height() << endl;
+            Triangle::info();
+        }
+    };
+
+    class RightTriangle : public Triangle
+    {
+        double base;
+        double height;
+    public:
+        RightTriangle(double base, double height, SHAPE_TAKE_PARAMETERS) : Triangle(SHAPE_GIVE_PARAMETERS)
+        {
+            set_base(base);
+            set_height(height);
+        }
+        void set_base(double base)
+        {
+            this->base = filter_size(base);
+        }
+        void set_height(double height)
+        {
+            this->height = filter_size(height);
+        }
+        double get_base() const
+        {
+            return base;
+        }
+        double get_height() const override
+        {
+            return height;
+        }
+        double get_area() const override
+        {
+            return base * height / 2;
+        }
+        double get_perimeter() const override
+        {
+            return base + height + sqrt(base * base + height * height);
+        }
+        void draw(HDC hdc) const override
+        {
+            HPEN hPen = CreatePen(PS_SOLID, line_width, color);
+            HBRUSH hBrush = CreateSolidBrush(color);
+
+            SelectObject(hdc, hPen);
+            SelectObject(hdc, hBrush);
+
+            POINT points[] =
+            {
+                {start_x, start_y + height},
+                {start_x + base, start_y + height},
+                {start_x, start_y}
+            };
+            Polygon(hdc, points, 3);
+
+            DeleteObject(hBrush);
+            DeleteObject(hPen);
+        }
+        void info() const override
+        {
+            cout << typeid(*this).name() << endl;
+            cout << "Основание прямоугольного треугольника: " << get_base() << endl;
+            cout << "Высота прямоугольного треугольника: " << get_height() << endl;
+            Triangle::info();
+        }
+    };
+}
+
+void DrawShapesToBitmap(const wchar_t* filename)
+{
+    int width = 1280;
+    int height = 960;
+
+    // Получаем контекст устройства для всего экрана
+    HDC hdc = GetDC(NULL);
+    // Создаем совместимый контекст устройства в памяти
+    HDC memDC = CreateCompatibleDC(hdc);
+    // Создаем совместимое с контекстом устройства изображение
+    HBITMAP hBitmap = CreateCompatibleBitmap(hdc, width, height);
+    // Выбираем изображение в контекст устройства
+    SelectObject(memDC, hBitmap);
+
+    // Заполняем фон белым цветом
+    HBRUSH hBrush = CreateSolidBrush(RGB(255, 255, 255));
+    RECT rect = { 0, 0, width, height };
+    FillRect(memDC, &rect, hBrush);
+    DeleteObject(hBrush);
+
+    // Создаем объекты фигур
+    Geometry::Square square(100, 100, 100, 5, Geometry::Color::RED);
+    Geometry::Rectangle rectShape(150, 80, 300, 100, 5, Geometry::Color::BLUE);
+    Geometry::Circle circle(50, 500, 100, 5, Geometry::Color::GREEN);
+    Geometry::EquilateralTriangle equilateralTriangle(150, 500, 250, 5, Geometry::Color::GREEN);
+    Geometry::IsoscelesTriangle isoscelesTriangle(100, 150, 100, 250, 5, Geometry::Color::RED);
+    Geometry::RightTriangle rightTriangle(100, 150, 300, 250, 5, Geometry::Color::BLUE);
+
+    // Рисуем фигуры на изображении
+    square.draw(memDC);
+    rectShape.draw(memDC);
+    circle.draw(memDC);
+    equilateralTriangle.draw(memDC);
+    isoscelesTriangle.draw(memDC);
+    rightTriangle.draw(memDC);
+
+    // Инициализируем структуры для заголовка BMP файла
+    BITMAPFILEHEADER bfHeader;
+    BITMAPINFOHEADER biHeader;
+    BITMAPINFO bInfo;
+    memset(&bfHeader, 0, sizeof(BITMAPFILEHEADER));
+    memset(&biHeader, 0, sizeof(BITMAPINFOHEADER));
+    memset(&bInfo, 0, sizeof(BITMAPINFO));
+
+    // Заполняем заголовок BMP файла
+    bfHeader.bfType = 0x4D42; // "BM"
+    bfHeader.bfOffBits = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
+    bfHeader.bfSize = bfHeader.bfOffBits + width * height * 4;
+
+    // Заполняем заголовок BMP информации
+    biHeader.biSize = sizeof(BITMAPINFOHEADER);
+    biHeader.biWidth = width;
+    biHeader.biHeight = -height; // Отражаем изображение по вертикали
+    biHeader.biPlanes = 1;
+    biHeader.biBitCount = 32;
+    biHeader.biCompression = BI_RGB;
+    biHeader.biSizeImage = width * height * 4;
+
+    bInfo.bmiHeader = biHeader;
+
+    // Создаем буфер для хранения пикселей изображения
+    DWORD dwBmpSize = width * height * 4;
+    HANDLE hDIB = GlobalAlloc(GHND, dwBmpSize);
+    char* lpbitmap = (char*)GlobalLock(hDIB);
+
+    // Получаем пиксели изображения из контекста устройства
+    GetDIBits(memDC, hBitmap, 0, (UINT)height, lpbitmap, &bInfo, DIB_RGB_COLORS);
+
+    // Записываем данные в файл
+    std::ofstream file(filename, std::ios::out | std::ios::binary);
+    file.write((char*)&bfHeader, sizeof(BITMAPFILEHEADER));
+    file.write((char*)&biHeader, sizeof(BITMAPINFOHEADER));
+    file.write(lpbitmap, dwBmpSize);
+    file.close();
+
+    // Освобождаем ресурсы
+    GlobalUnlock(hDIB);
+    GlobalFree(hDIB);
+
+    DeleteObject(hBitmap);
+    DeleteDC(memDC);
+    ReleaseDC(NULL, hdc);
+}
+
+void main()
+{
+    setlocale(LC_ALL, "");
+
+    const wchar_t* filename = L"shapes.bmp";
+
+    // Рисуем фигуры и сохраняем их в BMP файл
+    DrawShapesToBitmap(filename);
+
+    // Открываем созданный BMP файл в MS Paint
+    ShellExecute(NULL, L"open", L"mspaint.exe", filename, NULL, SW_SHOW);
+}
---
+++ ForvardList/main.cpp
@@ -0,0 +1,370 @@
+﻿#include<iostream>
+using namespace std;
+
+#define tab "\t"
+
+//======================1==================================
+
+class Element
+{
+	int Data;
+	Element* pNext;
+	static int count; 
+public:
+	Element(int Data, Element* pNext = nullptr) :Data(Data), pNext(pNext) 
+	{
+		count++;
+		//cout << "EConstructor:\t" << this << endl;
+	}
+	~Element()
+	{
+		count--;
+		//cout << "EDestructor:\t" << this << endl;
+	}
+	friend class ForwardList;
+	friend class Iterator;
+
+};
+int Element::count = 0;
+
+//============================================================
+
+class Iterator
+{
+	Element* Temp;
+public:
+	Iterator(Element* Temp = nullptr) :Temp(Temp)
+	{
+		cout << "ItConstructor:\t" << this << endl;
+	}
+	~Iterator()
+	{
+		cout << "ItDestructor:\t" << this << endl;
+}
+	Iterator& operator++()
+	{
+		Temp = Temp->pNext;
+		return *this;
+	}
+
+	bool operator!=(const Iterator& other)const
+	{
+		return this->Temp != other.Temp;
+	}
+	int operator*()
+	{
+		return Temp->Data;
+	}
+};
+
+//=======================2===================================
+
+
+class ForwardList // объединяет элементы в список
+  {
+	 
+	Element* Head;
+	unsigned int size;
+public:
+	Iterator begin()
+	{
+		return Head;
+	}
+	Iterator end()
+	{
+		return nullptr;
+	}
+
+	//                        Constructors:
+
+	ForwardList() // конструктор по умолчанию создает пустой список
+	{
+		Head = nullptr;// когда список пуст, его Голова указывает на НОЛЬ!
+		size = 0;
+		cout << "LConstructor: конструктор списка \t" << this << endl;
+	}
+	ForwardList(const std::initializer_list<int>il) :ForwardList()// преобразует initializerList в ForwardList
+	{
+		cout << typeid(il.begin()).name() << endl;
+		for (int const* it = il.begin(); it != il.end(); it++)
+		{
+			push_back(*it);
+		}
+	}
+	ForwardList(const ForwardList& other):ForwardList() //конструктор копирования всегда принимает константную ссылку на объект.!!!
+	{
+		//for (Element* Temp = other.Head; Temp; Temp = Temp->pNext)
+		//	push_back(Temp->Data);
+		*this = other; // после создания оператора присваивания = 
+		cout << "LCopyCpnstructor: " << this << endl;
+	}
+	ForwardList(ForwardList&& other)noexcept :ForwardList()
+	{
+		//this->Head = other.Head;
+		//this->size = other.size;
+		//other.Head = nullptr;
+		//other.size = 0;
+		*this = std::move(other);
+		cout << "LMoveConstructor:\t" << this << endl;
+	}
+	~ForwardList()
+	{
+		//while (Head)pop_back();
+		while (Head)pop_front();
+		cout << "LDestructor: \t" <<this<< endl;
+	}
+	//========== Operators ==========================
+	ForwardList& operator=(const ForwardList& other)
+	{
+		if(this == &other)return *this;
+		while (Head)pop_front();
+		for (Element* Temp = other.Head; Temp; Temp = Temp->pNext)
+			push_front(Temp->Data);
+		reverse();
+		cout << "LCopyAssignment: " << this << endl;
+		return *this;
+	}
+
+	
+	//метод добавляет значения в начало списка
+	// Adding elements:
+	void push_front(int Data)
+	{
+		Head = new Element(Data, Head); // сократилость в 1 строку
+		
+		//1 создаем новый элемент
+		//Element* New = new Element(Data);
+		//2 включаем новый элемент в список
+		//New->pNext = Head;
+		//3 переводим голову на новый элемент
+		//Head = New;
+		size++;
+	}
+	void push_back(int Data)
+	{
+		if (Head == nullptr)return push_front(Data);
+		
+		//1) Доходим до конца списка:
+		Element* Temp = Head;
+		while (Temp->pNext)
+		{
+			Temp = Temp->pNext;
+		}
+		Temp->pNext = new Element(Data); // сократили до одной строки
+		
+		//2 создаем новый элемент:
+		//Element* New = new Element(Data);
+		// 3 Включаем новый элемент в список:
+		//Temp->pNext = New;
+		size++;
+	}
+	void insert(int Data, int Index)
+	{
+		if (Index > Head->count)return;
+		if (Index == 0)return push_front(Data);
+		//доходим до нужного элемента
+		Element* Temp = Head;
+		for (int i = 0; i < Index - 1; i++)Temp = Temp->pNext;
+		
+		Temp->pNext = new Element(Data, Temp->pNext); // сокротили до строки
+
+		// 1 создаем новый элемент
+		//Element* New = new Element(Data);
+		//2 включаем элемент в список;
+		//New->pNext = Temp->pNext;
+		//Temp->pNext = New;
+		size++;
+	}
+	void erase(int Index)
+	{	
+		if (Index >= size)return;
+		if (Index == 0)return pop_front(); 
+		// доходим до элемента перед удаляем
+		Element* Temp = Head;
+		for (int i = 0; i < Index - 1; i++)Temp = Temp->pNext;
+		
+		//Запоминаем адрес удаляемого элемента 
+		Element* erased = Temp->pNext;
+		//исключаем удаляемый элемент из списка
+		Temp->pNext = Temp->pNext->pNext;
+		//Удаляем элкмент из памяти
+		delete erased;
+		size--;
+	}
+
+	void pop_front()
+	{
+		if (Head == nullptr)return;
+		Element* erased = Head; // запоминаем удаляемого элемента
+		Head = Head->pNext; //смещаем на голову вперед, исключаем элемент из списка
+		delete erased; // удаляем элемент из памяти
+		size--;
+	}
+	void pop_back()
+	{
+		if (Head == nullptr || Head->pNext == nullptr)return pop_front();
+		//if (Head == nullptr)return;
+		//if (Head->pNext == nullptr)return pop_front();
+		// 1  доходим до предпоследнего элемента списка
+		Element* Temp = Head;
+		while (Temp->pNext->pNext)Temp = Temp->pNext;
+		// удаляем последний элемент
+		delete Temp->pNext;
+		// 3 зануляем указатель на последний элемент:
+		Temp->pNext = nullptr;
+		size--;
+	}
+	void reverse()
+	{
+		ForwardList buffer;
+		while (Head)
+		{
+			buffer.push_front(Head->Data);
+			pop_front();
+		}
+		this->Head = buffer.Head;
+		this->size = buffer.size;
+		buffer.Head = nullptr;
+	}
+
+	//                 Methods:
+	void print()const
+	{
+		//cout << "Head:\t" << Head << endl;
+		//Element* Temp = Head; // Temp - это итератор
+		// Итератор - это указатель, при помощи корого можно перебирать элементы сруктуры данных
+		//while (Temp)
+		//{
+		//	cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+		//	Temp = Temp->pNext;//Переход на следующий элемент
+		//}
+		for (Element* Temp = Head; Temp; Temp = Temp->pNext)
+			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+		cout << "Количество элементов списка: " << size << endl;
+		cout << "Общее количество элементов списка: " << Element::count << endl;
+		//cout << "Общее количество элементов списка: " << Head->count << endl;
+	}
+};
+void Print(int arr[])
+{
+	cout << typeid(arr).name() << endl;
+	cout << sizeof(arr) << endl;
+	for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
+
+
+//==================================================================
+//#define BASE_CHECK
+//#define COUNT_CHECK
+//#define PERFORMANCE_CHECK
+//#define RANGE_BASED_FOR_ARRAY
+#define RANGE_BASED_FOR_LIST
+	void main()
+	{
+		setlocale(LC_ALL, "");
+
+#ifdef BASE_CHECK
+		//Element element(5);
+		int n;
+		cout << "Введите количество элементов: "; cin >> n;
+		ForwardList list;
+
+		for (int i = 0; i < n; i++)
+		{
+			list.push_back(rand() % 100);
+			//list.push_front(rand() % 100);
+		}
+
+		list.print();
+		/*list.push_back(123);
+		list.print();*/
+		//list.pop_front();
+		//list.print();
+		//list.pop_back();
+		//list.print();
+		int index;
+		int value;
+		cout << "Введите итдукс нового элемента: "; cin >> index;
+		cout << "Введите значение нового элемента: "; cin >> value;
+		list.insert(value, index);
+		list.print();
+
+		cout << "Введите индекс удаляемого элемента: "; cin >> index;
+		list.erase(index);
+		list.print();
+#endif // BASE_CHECK
+
+#ifdef COUNT_CHECK
+		ForwardList list1;
+		list1.push_back(3);
+		list1.push_back(5);
+		list1.push_back(8);
+		list1.push_back(13);
+		list1.push_back(21);
+		list1.print();
+
+		ForwardList list2;
+		list2.push_back(34);
+		list2.push_back(55);
+		list2.push_back(80);
+		list2.print();
+#endif // COUNT_CHECK
+
+
+#ifdef PERFORMANCE_CHECK
+		int n;
+		cout << "Введите количество элементов: "; cin >> n;
+		ForwardList list;
+
+		for (int i = 0; i < n; i++)
+		{
+			//list.push_back(rand() % 100);
+			list.push_front(rand() % 100);
+		}
+
+		cout << "List filled" << endl;
+		list.print();
+		cout << "Making Copy" << endl;
+		ForwardList list2 = list; // CopyConstructor
+		//ForwardList list2;
+		//list2 = list;
+		list2.print();
+		cout << "Copy Done" << endl;
+
+#endif // PERFORMANCE_CHECK
+
+#ifdef RANGE_BASED_FOR_ARRAY
+		int arr[] = { 3,5,8,13,21 };
+		for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+		{
+			cout << arr[i] << tab;
+		}
+		cout << endl;
+
+		/// https://legacy.cplusplus.com/doc/tutorial/control/#:~:text=equal%20to%2050.-,Range%2Dbased%20for%20loop,-The%20for%2Dloop
+			// Range-based for:
+			// Range(диапазон - промежуточных значений) в данном контексте понимется как контейнер.
+		for (int i : arr)
+		{
+			cout << i << tab;
+		}
+		cout << endl;
+		Print(arr);
+#endif // RANGE_BASED_FOR_ARRAY
+
+#ifdef RANGE_BASED_FOR_LIST
+		ForwardList list = { 3,5,8,13,21 };
+		//list.print();
+		for (int i : list)
+		{
+			cout << i << tab;
+		}
+		cout << endl;
+#endif // RANGE_BASED_FOR_LIST
+
+	}
+	
